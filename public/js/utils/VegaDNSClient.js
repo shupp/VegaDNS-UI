7be87ffbcfbb363c1eb73f0b646805873435702e
Bill Shupp <hostmaster@shupp.org>
2015-08-12 10:14:23 -0700

    Wrap ajax calls in common send method, first pass at using a q promise

--- public/js/utils/VegaDNSClient.js
+++ public/js/utils/VegaDNSClient.js
@@ -1,4 +1,5 @@
 var VegaDNSConfig = require('../utils/VegaDNSConfig');
+var Q = require('q');
 
 var VegaDNSClient = function() {
     var loggedIn = false;
@@ -12,30 +13,38 @@ VegaDNSClient.prototype.getHost = function(version=true) {
     return host;
 }
 
-VegaDNSClient.prototype.login = function(email, password) {
-    return $.ajax({
-        type: 'POST',
-        url: this.getHost() + "/login",
-        data: {
-            email: email,
-            password: password,
-        },
+VegaDNSClient.prototype.send = function(url, method, data) {
+    var deferred = Q.defer();
+    $.ajax({
+        type: method,
+        url: url,
+        data: data,
         xhrFields: {
             withCredentials: true
         },
-        dataType: 'json'
+        dataType: "json"
+    }).success(function(data) {
+        deferred.resolve(data);
+    }).error(function(data) {
+        deferred.reject(new Error('shitballs'));
     });
+
+    return deferred.promise;
+}
+
+VegaDNSClient.prototype.login = function(email, password) {
+    var url = this.getHost() + "/login";
+    var data = {
+        email: email,
+        password: password,
+    };
+
+    return this.send(url, "POST", data);
 }
 
 VegaDNSClient.prototype.healthcheck = function() {
-    return $.ajax({
-        type: 'GET',
-        url: this.getHost(false) + "/healthcheck",
-        dataType: 'json',
-        xhrFields: {
-            withCredentials: true
-        }
-    });
+    var url = this.getHost(false) + "/healthcheck";
+    return this.send(url, "GET");
 }
 
-module.exports = VegaDNSClient;
+module.exports = new VegaDNSClient();
