094a17d3d20263e04e148ceffa8b762c454d1a29
Bill Shupp <hostmaster@shupp.org>
2015-08-12 14:24:45 -0700

    Move sign out into new 'your account' dropdown

--- public/js/components/HeaderSection.react.js
+++ public/js/components/HeaderSection.react.js
@@ -5,7 +5,14 @@ var HeaderSection = React.createClass({
   render: function() {
 
       return (
-        <button className="btn btn-primary" onClick={this.submitLogOut}>Sign out</button>
+        <div className="dropdown">
+            <button class="btn btn-default dropdown-toggle" type="button" id="your-account-menu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Your Account <span className="caret"></span>
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="your-account-menu">
+                <li><a href="#" onClick={this.submitLogOut}>Sign Out</a></li>
+            </ul>
+        </div>
       );
   },
 
