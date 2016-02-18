c353774094470389d963c51091c721a50be75f07
Bill Shupp <hostmaster@shupp.org>
2016-02-18 12:46:09 -0800

    Row fixes for account edit

--- public/js/components/AccountEditForm.react.js
+++ public/js/components/AccountEditForm.react.js
@@ -40,61 +40,65 @@ var AccountEditForm = React.createClass({
 
     render: function() {
         return (
-            <section id="edit_account">
-                <h3 className="text-center">Edit Account</h3>
-                <form className="form-horizontal" autoComplete="off">
-                    <div className="form-group">
-                        <label htmlFor="first_name" className="col-sm-4 control-label">First Name</label>
-                        <div className="col-sm-8">
-                            <input onChange={this.handleChange.bind(this, 'first_name')} className="form-control" id="first_name" value={this.state.first_name} />
+            <div className="row">
+                <div className="col-md-12">
+                    <h3 className="text-center">Edit Account</h3>
+                </div>
+                <div className="col-md-12">
+                    <form className="form-horizontal" autoComplete="off">
+                        <div className="form-group">
+                            <label htmlFor="first_name" className="col-sm-4 control-label">First Name</label>
+                            <div className="col-sm-8">
+                                <input onChange={this.handleChange.bind(this, 'first_name')} className="form-control" id="first_name" value={this.state.first_name} />
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="last_name" className="col-sm-4 control-label">Last Name</label>
-                        <div className="col-sm-8">
-                            <input onChange={this.handleChange.bind(this, 'last_name')} className="form-control" id="last_name"  value={this.state.last_name} />
+                        <div className="form-group">
+                            <label htmlFor="last_name" className="col-sm-4 control-label">Last Name</label>
+                            <div className="col-sm-8">
+                                <input onChange={this.handleChange.bind(this, 'last_name')} className="form-control" id="last_name"  value={this.state.last_name} />
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email" className="col-sm-4 control-label">Email Address</label>
-                        <div className="col-sm-8">
-                            <input type="email" onChange={this.handleChange.bind(this, 'email')} className="form-control" autoComplete="off" value={this.state.email} />
+                        <div className="form-group">
+                            <label htmlFor="email" className="col-sm-4 control-label">Email Address</label>
+                            <div className="col-sm-8">
+                                <input type="email" onChange={this.handleChange.bind(this, 'email')} className="form-control" autoComplete="off" value={this.state.email} />
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password" className="col-sm-4 control-label">Change Password</label>
-                        <div className="col-sm-8">
-                            <input type="password" onChange={this.handleChange.bind(this, 'password')} className="form-control" />
+                        <div className="form-group">
+                            <label htmlFor="password" className="col-sm-4 control-label">Change Password</label>
+                            <div className="col-sm-8">
+                                <input type="password" onChange={this.handleChange.bind(this, 'password')} className="form-control" />
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="account_type" className="col-sm-4 control-label">User Type</label>
-                        <div className="col-sm-3">
-                            <select id="account_type" onChange={this.handleChange.bind(this, 'account_type')} className="form-control" value={this.state.account_type}>
-                                <option value="senior_admin">Senior Admin</option>
-                                <option value="user">User</option>
-                            </select>
+                        <div className="form-group">
+                            <label htmlFor="account_type" className="col-sm-4 control-label">User Type</label>
+                            <div className="col-sm-3">
+                                <select id="account_type" onChange={this.handleChange.bind(this, 'account_type')} className="form-control" value={this.state.account_type}>
+                                    <option value="senior_admin">Senior Admin</option>
+                                    <option value="user">User</option>
+                                </select>
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="status" className="col-sm-4 control-label">Account Status</label>
-                        <div className="col-sm-3">
-                            <select id="status" onChange={this.handleChange.bind(this, 'status')} className="form-control" value={this.state.status}>
-                                <option value="active">Active</option>
-                                <option value="inactive">Inactive</option>
-                            </select>
+                        <div className="form-group">
+                            <label htmlFor="status" className="col-sm-4 control-label">Account Status</label>
+                            <div className="col-sm-3">
+                                <select id="status" onChange={this.handleChange.bind(this, 'status')} className="form-control" value={this.state.status}>
+                                    <option value="active">Active</option>
+                                    <option value="inactive">Inactive</option>
+                                </select>
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-sm-4"></div>
-                        <div className="col-sm-8">
-                            <button type="submit" onClick={this.editAccount} className="btn btn-primary">Edit</button>
-                            &nbsp;
-                            <button type="submit" onClick={this.props.cancelCallback} className="btn btn-danger">Cancel</button>
+                        <div className="form-group">
+                            <div className="col-sm-4"></div>
+                            <div className="col-sm-8">
+                                <button type="submit" onClick={this.editAccount} className="btn btn-primary">Edit</button>
+                                &nbsp;
+                                <button type="submit" onClick={this.props.cancelCallback} className="btn btn-danger">Cancel</button>
+                            </div>
                         </div>
-                    </div>
-                </form>
-            </section>
+                    </form>
+                </div>
+            </div>
         );
     }
 });
