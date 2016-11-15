cd1ce1ee7c391e267417669074fb5514e214035e
Bill Shupp <bill@shupp.org>
2016-11-15 09:48:08 -0800

    Added errorNotification and successNotification shortcut actions

--- public/js/actions/VegaDNSActions.js
+++ public/js/actions/VegaDNSActions.js
@@ -365,6 +365,26 @@ var VegaDNSActions = {
             actionType: VegaDNSConstants.DISMISS_NOTIFICATION
         });
     },
+    errorNotification: function(defaultMessage, data=null, autoDismiss=false) {
+        if (data != null) {
+            if (typeof data.responseJSON != "undefined" && typeof data.responseJSON.message !== "undefined") {
+                defaultMessage += data.responseJSON.message;
+            }
+        }
+
+        this.addNotification(
+            VegaDNSConstants.NOTIFICATION_DANGER,
+            defaultMessage,
+            autoDismiss
+        );
+    },
+    successNotification: function(message, autoDismiss=true) {
+        this.addNotification(
+            VegaDNSConstants.NOTIFICATION_SUCCESS,
+            message,
+            autoDismiss
+        );
+    },
     redirect: function(route) {
         AppDispatcher.dispatch({
             actionType: VegaDNSConstants.REDIRECT_ROUTE,
